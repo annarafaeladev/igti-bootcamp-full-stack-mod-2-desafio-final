5d5239bad8ab94dc0677dcc90a79427549bea390
rafabispo53 <rafabispo53@gmail.com>
2020-06-09 01:51:51 -0300

    update routes file json, add endpoints and adjust path.resolve

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,13 +3,21 @@ const router = express.Router();
 const fs = require("fs").promises;
 const path = require("path");
 
-const pathJson = path.resolve(__dirname, "grades.json");
+const pathJson = path.resolve(__dirname, "../", "grades.json");
+
+async function readFileJson() {
+  try {
+    let fileJson = await fs.readFile(pathJson, "utf8");
+    fileJson = JSON.parse(fileJson);
+    return fileJson;
+  } catch (err) {
+    return err;
+  }
+}
 
 router.get("/grade/index", async (_, res) => {
   try {
-    let fileJson = null;
-    fileJson = await fs.readFile(pathJson);
-    fileJson = JSON.parse(fileJson);
+    let fileJson = await readFileJson();
     return res.json({ ok: true, result: fileJson });
   } catch (error) {
     return res.json({ ok: false, result: null });
@@ -19,9 +27,7 @@ router.get("/grade/index", async (_, res) => {
 router.post("/grade/create", async (req, res) => {
   const { student, subject, type, value } = req.body;
   try {
-    let fileJson = null;
-    fileJson = await fs.readFile(pathJson, "utf8");
-    fileJson = JSON.parse(fileJson);
+    let fileJson = await readFileJson();
     let data = {
       id: fileJson.nextId++,
       student: student,
@@ -49,9 +55,7 @@ router.post("/grade/create", async (req, res) => {
 router.put("/grade/edit", async (req, res) => {
   const { student, subject, type, value, id } = req.body;
   try {
-    let fileJson = null;
-    fileJson = await fs.readFile(pathJson, "utf8");
-    fileJson = JSON.parse(fileJson);
+    let fileJson = await readFileJson();
     let index = fileJson.grades.findIndex((grade) => grade.id === id);
     if (index) {
       (fileJson.grades[index].student = student),
@@ -71,14 +75,15 @@ router.put("/grade/edit", async (req, res) => {
 
 router.delete("/grade/delete/:id", async (req, res) => {
   try {
-    let fileJson = null;
-    fileJson = await fs.readFile(pathJson, "utf8");
-    fileJson = JSON.parse(fileJson);
+    let fileJson = await readFileJson();
+
     let data = fileJson.grades.filter(
       (grade) => grade.id !== parseInt(req.params.id)
     );
 
-    await fs.writeFile(pathJson, JSON.stringify(data));
+    fileJson.grades = data;
+    await fs.writeFile(pathJson, JSON.stringify(fileJson));
+
     res.json({
       ok: true,
       message: "Removed Success",
@@ -91,8 +96,7 @@ router.delete("/grade/delete/:id", async (req, res) => {
 router.get("/grade/:id", async (req, res) => {
   let id = req.params.id;
   try {
-    let fileJson = null;
-    fileJson = await fs.readFile(pathJson, "utf8");
+    let fileJson = await readFileJson();
     fileJson = JSON.parse(fileJson);
     let data = fileJson.grades.filter((grade) => grade.id === parseInt(id, 10));
 
@@ -118,29 +122,89 @@ router.get("/grade/:id", async (req, res) => {
   }
 });
 
-router.get("/teste", async (req, res) => {
+router.get("/soma", async (req, res) => {
   const { student, subject } = req.query;
-  let fileJson = null;
-  fileJson = await fs.readFile(pathJson, "utf8");
-  fileJson = JSON.parse(fileJson);
+  try {
+    let fileJson = await readFileJson();
+    let studentName = fileJson.grades.filter(
+      (grade) => grade.student === student
+    );
+    let sum = 0;
 
-  let studentName = fileJson.grades.filter(
-    (grade) => grade.student === student
-  );
+    let subjectStudent = studentName.filter((student) => {
+      if (student.subject === subject) {
+        return (sum += student.value);
+      }
+    });
 
-  let sum = 0;
-
-  let subjectStudent = studentName.filter((student) => {
-    if (student.subject === subject) {
-      return (sum += student.value);
-    }
-  });
-
-  res.json({
-    ok: true,
-    subject: subjectStudent,
-    result: `Soma das notas subject: ${subject} =  ${sum}`,
-  });
+    res.json({
+      ok: true,
+      subject: subjectStudent,
+      result: `Soma das notas subject: ${subject} =  ${sum}`,
+    });
+  } catch (error) {
+    res.json({
+      ok: false,
+      result: null,
+    });
+  }
 });
 
+router.get("/media", async (req, res) => {
+  const { type, subject } = req.query;
+  try {
+    let fileJson = await readFileJson();
+    let typeParam = fileJson.grades.filter((grade) => grade.type === type);
+    let sum = 0;
+    let count = 0;
+    let subjectType = typeParam.filter((grade) => {
+      if (grade.subject === subject) {
+        count++;
+        return (sum += grade.value);
+      }
+    });
+
+    res.json({
+      ok: true,
+      subject: subjectType,
+      result: `Média: ${subject} do tipo ${type}=  ${sum / count}`,
+    });
+  } catch (error) {
+    res.json({
+      ok: false,
+      result: null,
+    });
+  }
+});
+
+router.get("/melhor-grade", async (req, res) => {
+  const { type, subject } = req.query;
+  try {
+    let fileJson = await readFileJson();
+
+    let typeData = fileJson.grades.filter((grade) => grade.type === type);
+
+    let subjectData = typeData.filter((grade) => grade.subject === subject);
+    subjectData = subjectData.sort((a, b) => b.value - a.value);
+
+    let initialID = 1;
+    let result = [];
+    subjectData.filter((grade) => {
+      if (initialID < 4) {
+        result.push({ ...grade });
+        initialID++;
+      }
+    });
+
+    res.json({
+      ok: true,
+      result: result,
+    });
+  } catch (error) {
+    res.json({
+      ok: false,
+      result: null,
+    });
+  }
+});
 module.exports = router;
