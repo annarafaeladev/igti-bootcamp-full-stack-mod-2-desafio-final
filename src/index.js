b214c18e4dbba6c726935ae152253c24c43d2c94
rafabispo53 <rafabispo53@gmail.com>
2020-06-09 01:50:10 -0300

    removed fs, adjust path routes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 const express = require("express");
-const fs = require("fs").promises;
 const app = express();
 
-const routes = require("./routes/routes");
+const routes = require("./routes/routes.js");
 app.use(express.json());
 app.use("/", routes);
 
